2b082b25b5292655ae90af5229f4422d7c925069
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2023-12-22 20:27:42 -0800

    Update page.py

--- page.py
+++ page.py
@@ -104,40 +104,23 @@ with st.sidebar:
     with r5c2:
         tail_interval_input = st.selectbox("Tail Interval", ["Week", "Month"], key="tail_interval", index=0)
 
-
-symbols = input_string.upper().split(",")
-benchmark = benchmark_input.split(",")[0].upper()
-study = study_input.lower()
-long_period = long_period_input
-short_period = short_period_input
-show_tails=show_tails_input
-tail_periods = tail_periods_input
-tail_interval = tail_interval_input.lower()
-window = window_input
-trading_periods = trading_periods_input
-date = date_input
-source = source_dict[source_input]
-
-
-def create(symbols, benchmark, study, date, long_period, short_period, window, trading_periods, tail_periods, tail_interval, provider):
-    result = asyncio.run(module.create(
-        symbols = symbols,
-        benchmark = benchmark,
-        study = study,
-        date = pd.to_datetime(date),
-        long_period = long_period,
-        short_period = short_period,
-        window = window,
-        trading_periods = trading_periods,
-        tail_periods = tail_periods,
-        tail_interval = tail_interval,
-        provider=provider
-    ))
-    return result
-
-if st.session_state.button_clicked:
-    try:
-        rrg_data = asyncio.run(module.create(
+def main():
+    symbols = input_string.upper().split(",")
+    benchmark = benchmark_input.split(",")[0].upper()
+    study = study_input.lower()
+    long_period = long_period_input
+    short_period = short_period_input
+    show_tails=show_tails_input
+    tail_periods = tail_periods_input
+    tail_interval = tail_interval_input.lower()
+    window = window_input
+    trading_periods = trading_periods_input
+    date = date_input
+    source = source_dict[source_input]
+    
+    
+    def create(symbols, benchmark, study, date, long_period, short_period, window, trading_periods, tail_periods, tail_interval, provider):
+        result = asyncio.run(module.create(
             symbols = symbols,
             benchmark = benchmark,
             study = study,
@@ -148,97 +131,117 @@ if st.session_state.button_clicked:
             trading_periods = trading_periods,
             tail_periods = tail_periods,
             tail_interval = tail_interval,
-            provider=source,
+            provider=provider
         ))
-        st.session_state.rrg_data = rrg_data
-        st.session_state.first_run = False
-    except Exception:
-        st.session_state.rrg_data = None
+        return result
+    
+    if st.session_state.button_clicked:
+        try:
+            rrg_data = asyncio.run(module.create(
+                symbols = symbols,
+                benchmark = benchmark,
+                study = study,
+                date = pd.to_datetime(date),
+                long_period = long_period,
+                short_period = short_period,
+                window = window,
+                trading_periods = trading_periods,
+                tail_periods = tail_periods,
+                tail_interval = tail_interval,
+                provider=source,
+            ))
+            st.session_state.rrg_data = rrg_data
+            st.session_state.first_run = False
+        except Exception:
+            st.session_state.rrg_data = None
+            st.session_state.first_run = True
+            if input_string != "" and benchmark_input != "":
+                st.write("There was an error fetching the data.  Please check if the symbols are correct and are available from the selected source.")
+            if input_string == "" or benchmark_input == "":
+                st.write("Please enter a list of symbols and a benchmark.")
+    
+    
+    main_chart = st.expander("Relative Rotation Graph", expanded=True)
+    
+    if "first_run" not in st.session_state:
         st.session_state.first_run = True
-        if input_string != "" and benchmark_input != "":
-            st.write("There was an error fetching the data.  Please check if the symbols are correct and are available from the selected source.")
-        if input_string == "" or benchmark_input == "":
-            st.write("Please enter a list of symbols and a benchmark.")
-
-
-main_chart = st.expander("Relative Rotation Graph", expanded=True)
-
-if "first_run" not in st.session_state:
-    st.session_state.first_run = True
-
-if not st.session_state.first_run and st.session_state.rrg_data is not None:
-    with main_chart:
-        fig = st.session_state.rrg_data.show(date, show_tails, tail_periods, tail_interval)
-        fig.update_layout(height=600)
-        st.session_state.fig = fig
-        st.plotly_chart(
-            st.session_state.fig,
-            use_container_width=True,
-            config={
-                "scrollZoom": True,
-                "modeBarButtonsToRemove": ["lasso2d", "select2d"],
-                "modeBarButtons": [
-                    ["toImage"],
-                    [
-                        "drawline",
-                        "drawopenpath",
-                        "drawcircle",
-                        "drawrect",
-                        "eraseshape",
+    
+    if not st.session_state.first_run and st.session_state.rrg_data is not None:
+        with main_chart:
+            fig = st.session_state.rrg_data.show(date, show_tails, tail_periods, tail_interval)
+            fig.update_layout(height=600)
+            st.session_state.fig = fig
+            st.plotly_chart(
+                st.session_state.fig,
+                use_container_width=True,
+                config={
+                    "scrollZoom": True,
+                    "modeBarButtonsToRemove": ["lasso2d", "select2d"],
+                    "modeBarButtons": [
+                        ["toImage"],
+                        [
+                            "drawline",
+                            "drawopenpath",
+                            "drawcircle",
+                            "drawrect",
+                            "eraseshape",
+                        ],
+                        ["zoomIn2d", "zoomOut2d", "autoScale2d", "zoom2d", "pan2d"],
                     ],
-                    ["zoomIn2d", "zoomOut2d", "autoScale2d", "zoom2d", "pan2d"],
-                ],
-            }
-        )
-        st.markdown("""
-            <style>
-            .js-plotly-plot .plotly .modebar {
-                top: 610px !important;
-                bottom: auto !important;
-                transform: translateY(0) !important;
-            }
-            </style>
-            """, unsafe_allow_html=True)
+                }
+            )
+            st.markdown("""
+                <style>
+                .js-plotly-plot .plotly .modebar {
+                    top: 610px !important;
+                    bottom: auto !important;
+                    transform: translateY(0) !important;
+                }
+                </style>
+                """, unsafe_allow_html=True)
+    
+    
+        with st.expander("Study Data Table", expanded=False):
+            symbols_data = (
+                basemodel_to_df(st.session_state.rrg_data.symbols_data)
+                .join(basemodel_to_df(st.session_state.rrg_data.benchmark_data)[st.session_state.rrg_data.benchmark])
+            ).set_index("date")
+            symbols_data.index = pd.to_datetime(symbols_data.index).strftime("%Y-%m-%d")
+            st.dataframe(symbols_data)
+    
+        with st.expander("Relative Strength Ratio Table", expanded=False):
+            ratios_data = basemodel_to_df(st.session_state.rrg_data.rs_ratios).set_index("date")
+            ratios_data.index = pd.to_datetime(ratios_data.index).strftime("%Y-%m-%d")
+            st.dataframe(ratios_data)
+    
+        with st.expander("Relative Strength Momentum Table", expanded=False):
+            ratios_data = basemodel_to_df(st.session_state.rrg_data.rs_momentum).set_index("date")
+            ratios_data.index = pd.to_datetime(ratios_data.index).strftime("%Y-%m-%d")
+            st.dataframe(ratios_data)
+    
+    with st.expander("About"):
+        st.write("""
+            This dashboard is powered by the OpenBB Platform. More information can be found here: https://docs.openbb.co/platform
+    
+            A Relative Rotation Graph is a study of the Relative Strength Ratio vs. Relative Strength Momentum against a
+            benchmark. They are lagging indicators and are typically used for comparing sector or index
+            cconstituents.
+    
+            The Relative Strength Ratio is the price (volume or realized volatility) of the asset divided by the
+            benchmark.
+    
+            The Relative Strength (RS) Momentum is the momentum of the Relative Strength Ratio.
+            In this application, momentum is calculated as the trailing 12-month minus 1-month return.
+            The default values for long and short periods are 252 and 21, which is the number of trading
+            days in a year and a month, respectively. These values can be changed in the sidebar.
+    
+            Realized volatility is calculated as the annualized standard deviation over a trailing 1-month period.
+            The default values of 252 and 21, respectively, can be changed in the sidebar.
+    
+            All calculations are from daily values from the source selected in the sidebar. It should not be assumed
+            that volume represents 100% market coverage. This dashboard is for demonstration purposes only and
+            should not be used to make inferences or investment decisions.
+        """)
 
-
-    with st.expander("Study Data Table", expanded=False):
-        symbols_data = (
-            basemodel_to_df(st.session_state.rrg_data.symbols_data)
-            .join(basemodel_to_df(st.session_state.rrg_data.benchmark_data)[st.session_state.rrg_data.benchmark])
-        ).set_index("date")
-        symbols_data.index = pd.to_datetime(symbols_data.index).strftime("%Y-%m-%d")
-        st.dataframe(symbols_data)
-
-    with st.expander("Relative Strength Ratio Table", expanded=False):
-        ratios_data = basemodel_to_df(st.session_state.rrg_data.rs_ratios).set_index("date")
-        ratios_data.index = pd.to_datetime(ratios_data.index).strftime("%Y-%m-%d")
-        st.dataframe(ratios_data)
-
-    with st.expander("Relative Strength Momentum Table", expanded=False):
-        ratios_data = basemodel_to_df(st.session_state.rrg_data.rs_momentum).set_index("date")
-        ratios_data.index = pd.to_datetime(ratios_data.index).strftime("%Y-%m-%d")
-        st.dataframe(ratios_data)
-
-with st.expander("About"):
-    st.write("""
-        This dashboard is powered by the OpenBB Platform. More information can be found here: https://docs.openbb.co/platform
-
-        A Relative Rotation Graph is a study of the Relative Strength Ratio vs. Relative Strength Momentum against a
-        benchmark. They are lagging indicators and are typically used for comparing sector or index
-        cconstituents.
-
-        The Relative Strength Ratio is the price (volume or realized volatility) of the asset divided by the
-        benchmark.
-
-        The Relative Strength (RS) Momentum is the momentum of the Relative Strength Ratio.
-        In this application, momentum is calculated as the trailing 12-month minus 1-month return.
-        The default values for long and short periods are 252 and 21, which is the number of trading
-        days in a year and a month, respectively. These values can be changed in the sidebar.
-
-        Realized volatility is calculated as the annualized standard deviation over a trailing 1-month period.
-        The default values of 252 and 21, respectively, can be changed in the sidebar.
-
-        All calculations are from daily values from the source selected in the sidebar. It should not be assumed
-        that volume represents 100% market coverage. This dashboard is for demonstration purposes only and
-        should not be used to make inferences or investment decisions.
-    """)
+if __name__ == "__main__":
+    main()
